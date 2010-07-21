73c5bd4dd0f8efe61f386204995533c95dd5febc
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2010-07-21 17:13:30 +0100

    Bug in setup.py was breaking the scripts.

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ Syntax [DRS]_.  MIP table support is based on CMOR-%(cmor2_version)s.
         'metaconfig',
       ],
       entry_points= {
-        'console_scripts': ['translate_cmip3 = isenes.drslib.translate_cmip3:main',
-                            'drs_tree = isenes.drslib.drs_command:main'],
+        'console_scripts': ['translate_cmip3 = drslib.translate_cmip3:main',
+                            'drs_tree = drslib.drs_command:main'],
         },
       test_suite='nose.collector',
       )
