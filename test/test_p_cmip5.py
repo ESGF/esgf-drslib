7001a74c49d6aa4e89ad240cb82da6fe7ff00ccd
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2010-09-29 13:59:32 +0100

    First 6 p_cmip5 tests aggree with Martin's test script

--- test/test_p_cmip5.py
+++ test/test_p_cmip5.py
@@ -4,96 +4,92 @@ This test module uses the functional test features of nose.
 """
 
 import tempfile, os, shutil
+import zipfile
 
 import drslib.p_cmip5.product as p
 from drslib.p_cmip5 import init
-import gen_drs
 
 from nose import with_setup
 
-pc = None
+pc1 = None
+pc2 = None
 tmpdir = None
 
 
 def setup_module():
-    global pc, tmpdir
+    global pc1, pc2, tmpdir
     tmpdir = tempfile.mkdtemp(prefix='p_cmip5-')
     shelve_dir = os.path.join(tmpdir, 'sh')
 
     # Example config file is in test directory
     test_dir = os.path.dirname(__file__)
-    config = os.path.join(test_dir, 'ukmo_sample.ini')
+    config1 = os.path.join(test_dir, 'sample_1.ini')
+    config2 = os.path.join(test_dir, 'sample_2.ini')
 
     # Shelves are regenerated each time.  This could be optimised.
     shelves = init.init(shelve_dir)
 
     # Create the dummy data
-    dummy_list = os.path.join(test_dir, 'p_cmip5_dummy.ls')
-    gen_drs.write_listing(tmpdir, dummy_list)
+    z = zipfile.ZipFile(os.path.join(test_dir, 'dummy_archive.zip'))
+    z.extractall(path=tmpdir)
 
-    pc = p.cmip5_product(mip_table_shelve=shelves['stdo_mip'],
-                         template=shelves['template'],
-                         stdo=shelves['stdo'],
-                         config=config)
+    pc1 = p.cmip5_product(mip_table_shelve=shelves['stdo_mip'],
+                          template=shelves['template'],
+                          stdo=shelves['stdo'],
+                          config=config1)
+    pc2 = p.cmip5_product(mip_table_shelve=shelves['stdo_mip'],
+                          template=shelves['template'],
+                          stdo=shelves['stdo'],
+                          config=config2)
 
 
 def teardown_module():
     shutil.rmtree(tmpdir)
 
 
-def get_product(var, mip, expt,startyear,endyear,
-                path = ''):
-    path = os.path.join(tmpdir, path)
+
+
+def do_product(var, mip, expt,startyear=None,endyear=None,path = 'tmp', 
+            pci=None,verbose=False, tab='    '):
     model = 'HADCM3'
-    if pc.find_product(var, mip, expt, model, path,
-                       startyear=startyear,endyear=endyear):
-        print var,',',mip,',',expt,startyear,endyear,':: ', pc.product, pc.reason
-        return pc.product
+
+    path = os.path.join(tmpdir, path)
+    if pci is None:
+        pci = pc1
+
+    if startyear == None:
+        if pci.find_product_ads( var, mip, expt,model,path, verbose=verbose):
+            print tab,var,',',mip,',',expt,path,':: ',pci.product, pci.reason 
+        if pci.product == 'split':
+            if len( pci.output1_files ) == 0:
+                print tab,' ***** NO FILES FOUND'
+                assert False
+            else:
+                print tab,'    --> output1: %s .... %s' % (pci.output1_files[0],pci.output1_files[-1])
+        else:
+            print tab,'FAILED:: ',pci.status,':: ',var,',',mip,',',expt
+            assert False
     else:
-        print 'FAILED:: ',pc.status,':: ',var,',',mip,',',expt
-        assert False
+        if pci.find_product( var, mip, expt,model,path,startyear=startyear, verbose=verbose):
+            print tab,var,',',mip,',',expt,path,startyear,':: ',pci.product, pci.reason 
+        else:
+            print tab,'FAILED:: ',pci.status,':: ',var,',',mip,',',expt
+            assert False
+    if pci.warning != '':
+        print tab, 'WARNING:: ', pci.warning
+
+    return pci.product
+
+
 
 def check_product(params, expect):
-    assert get_product(*params) == expect
+    assert do_product(*params) == expect
 
-def test_product():
-    """
-    Generator of multiple tests.
-
-    """
+def test_gen():
     for var in ['tas','pr','ua']:
-        for mip in ['3hr', 'day']:
-            if mip == '3hr':
+        for mip in ['3hr','day']:
+            if var == 'ua' and mip == '3hr':
                 expect = 'output2'
             else:
                 expect = 'output1'
-            yield check_product, (var, mip, 'rcp45',2050, 2050), expect
-
-path = 'tas/r2p1i1/'
-path3 = 'tas/r3p1i1/'
-def test1():
-    assert get_product('tas', '3hr', 'rcp45', 
-                       startyear=2090, endyear=2090) == 'output1'
-def test2():
-    assert get_product('tas', '3hr', 'piControl', 
-                       startyear=2090, endyear=2090, path=path) == 'output2'
-def test3():
-    assert get_product('tas', '3hr', 'piControl',
-                       startyear=2090, endyear=2090, path=path3 ) == 'output1'
-def test4():
-    assert get_product('tas', '3hr', 'historical',
-                        startyear=1990, endyear=1990 ) == 'output1'
-def test5():
-    assert get_product('intpdiaz', 'Omon', 'rcp45',
-                        startyear=2050,endyear=2050 ) == 'output1'
-def test6():
-    assert get_product('intpp', 'Omon', 'rcp45',
-                       startyear=2050,endyear=2050 ) == 'output1'
-def test7():
-    # Will fail!
-    assert get_product( 'sconcno3', 'aero', 'piControl', 
-                        startyear=2050,endyear=2050 ) == 'output2'
-def test8():
-    # Will fail!
-    assert get_product( 'rlu', 'cfMon', 'AMIP', 
-                        startyear=2000, endyear=2000 ) == 'output2'
+            yield check_product, (var, mip, 'rcp45', 2050, 2050, 'tmp/a_2010_2020'), expect
