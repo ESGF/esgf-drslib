a25404618a27954e96ec87c584a771febab383da
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2011-01-14 14:50:03 +0000

    Added test to detect a bug in drs_tree's product detection where if a set of files belong in multiple products it will try and put all files into both products.

---
+++ test/test_drs_tree_product.py
@@ -0,0 +1,73 @@
+
+from drslib.cmip5 import make_translator
+from drslib import p_cmip5
+from drslib import drs_tree
+import gen_drs
+
+import os, shutil
+import metaconfig
+import tempfile
+
+config = metaconfig.get_config('drslib')
+shelve_dir = config.get('p_cmip5', 'shelve-dir')
+drs_root = '/cmip5'
+vtrans = make_translator(drs_root)
+
+def setup_module():
+    global p_cmip5, listing, tmpdir, dt
+
+    tmpdir = tempfile.mkdtemp(prefix='drs_tree-product-')
+
+    print 'TMPDIR  ',tmpdir
+    shelves = p_cmip5.init._find_shelves(shelve_dir)
+
+    config_file = os.path.join(os.path.dirname(__file__), 'ukmo_sample.ini')
+    listing = os.path.join(os.path.dirname(__file__), 'mohc_product_dup.ls')
+    gen_drs.write_listing(tmpdir, listing)
+    
+    p_cmip5 = p_cmip5.product.cmip5_product(mip_table_shelve=shelves['stdo_mip'],
+                                            template=shelves['template'],
+                                            stdo=shelves['stdo'],
+                                            config=config_file,
+                                            not_ok_excpt=True)
+
+    dt = drs_tree.DRSTree(tmpdir)
+    dt.set_p_cmip5(p_cmip5)
+
+def iter_files():
+    filenames = open(listing).readlines()
+    for filename in filenames:
+        filename = filename.strip()
+        yield filename, tmpdir
+
+
+
+def test_product_dup():
+    """
+    Test scanning a set of files that are put into multiple products.
+
+    """
+
+    dt.discover_incoming_fromfiles(iter_files())
+
+    # There should be 2 pub-level datasets
+    assert len(dt.pub_trees) == 2
+
+    pt1, pt2 = dt.pub_trees.values()
+
+    # They should be in separate products
+    assert pt1.drs.product != pt2.drs.product
+
+    #!NOTE: if the test fails before here it isn't really testing multi-product ingests
+    #    probably the p_cmip5 algorithm has changed to put the test data into 1 product.
+
+    # They should be disjoint
+    set1 = set(x[0] for x in pt1._todo)
+    set2 = set(x[0] for x in pt2._todo)
+    assert set1.isdisjoint(set2)
+
+def teardown_module():
+    shutil.rmtree(tmpdir)
+    
+
+
