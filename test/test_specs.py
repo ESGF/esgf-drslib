be6a3b31f82874e9b177a1ff0a638ceb45a87e25
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2014-10-01 12:44:42 +0100

    Basic tests for SPECS

--- test/test_specs.py
+++ test/test_specs.py
@@ -82,47 +82,48 @@ def test_4():
     assert drs.subset[0][:2] == (1962, 01)
     assert drs.subset[1][:2] == (1971, 12)
 
-# class TestSpecsListing1(TestListing):
-#     __test__ = True
+class TestSpecsListing1(TestListing):
+    __test__ = True
 
-#     listing_file = 'specs_test_EUR-44.ls'
+    listing_file = 'specs_1.ls'
 
-#     def setUp(self):
-#         super(TestSpecsListing1, self).setUp()
+    def setUp(self):
+        super(TestSpecsListing1, self).setUp()
 
-#         # incoming is not tmpdir/output.
-#         self.incoming = self.tmpdir
+        # incoming is not tmpdir/output.
+        self.incoming = self.tmpdir
 
-#     def _init_drs_fs(self):
-#         self.drs_fs = SpecsFileSystem(self.tmpdir)
+    def _init_drs_fs(self):
+        self.drs_fs = SpecsFileSystem(self.tmpdir)
 
-#     def test_1(self):
-#         self.dt.discover(self.incoming, activity='specs',
-#                          product='output')
+    def test_1(self):
+        self.dt.discover(self.incoming, activity='specs',
+                         product='output',
+                         realm='ocean',
+                         frequency='mon',
+                         institute='IPSL',
+                         )
 
-#         print len(self.dt.pub_trees)
-#         assert len(self.dt.pub_trees) == 168
+        print len(self.dt.pub_trees)
+        assert len(self.dt.pub_trees) == 3
 
-#     def test_2(self):
-#         self.dt.discover(self.incoming, activity='specs',
-#                          product='output',
-#                          frequency='day')
 
-#         print len(self.dt.pub_trees)
-#         assert len(self.dt.pub_trees) == 50
+    def test_2(self):
+        self.dt.discover(self.incoming, activity='specs',
+                         product='output',
+                         realm='ocean',
+                         frequency='mon',
+                         institute='IPSL',
+                         )
 
-#     def test_3(self):
-#         self.dt.discover(self.incoming, activity='specs',
-#                          product='output',
-#                          frequency='day',
-#                          variable='vas')
+        print len(self.dt.pub_trees)
+        assert len(self.dt.pub_trees) == 3
 
-#         print len(self.dt.pub_trees)
-#         assert len(self.dt.pub_trees) == 1
+        pt = self.dt.pub_trees.values()[0]
+        self._do_version(pt)
 
-#         pt = self.dt.pub_trees.values()[0]
-#         self._do_version(pt)
-
+        pt.deduce_state()
+        assert len(pt.versions.values()[0]) == 1
 
 
 
